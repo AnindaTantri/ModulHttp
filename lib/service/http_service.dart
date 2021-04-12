0c6fb0147a54357bc6bc9b4207f6d7cc2cf73a36
= <=>
2021-04-12 13:31:17 +0700

    membt model response http & hlm list populermovie

---
+++ lib/service/http_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:httprequest/models/models.dart';
+
+class HttpService {
+  final String apiKey = '621180ba8d01d949ad2c2b21b55e444d';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+
+  Future<List> getPopularMovies() async {
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Fail");
+      return null;
+    }
+  }
+}
